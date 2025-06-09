78996f5328a2f7f991ca50e3f1ad3ba8fffeab95
jesse waweru <jessewaweru1@gmail.com>
2025-06-09 13:10:41 +0300

    changes to loginregister.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import "./styles/loginregister.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
